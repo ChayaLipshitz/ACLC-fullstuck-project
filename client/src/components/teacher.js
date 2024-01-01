8ff823a053a581b318ea8821ab1be315e7e2a2ac
ChayaLipshitz <chl4165772@gmail.com>
2024-01-01 10:52:25 +0200

    Update teacher component

--- client/src/components/teacher.js
+++ client/src/components/teacher.js
@@ -30,7 +30,7 @@ export default function Teacher() {
 
     return (
         <>            
-            <h1>Good day </h1><Details user={user}/><h1>!!!</h1>
+            <h1>Hello  </h1><Details user={user}/>
             {courses.length>0 && <div>your courses:</div>}
             {courses.length>0 && courses.map(item => { return <NavLink activeclassname="active" key={counter++} className="nav-link" exact="true" to={`/teacher/${item.courseName}`} ><h3>{item.courseName}</h3></NavLink> })}
             <Routes>
